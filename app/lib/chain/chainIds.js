fdc550ee1b4e36df3ce528b9bbe73240a244526e
ioBanker <37595908+ioBanker@users.noreply.github.com>
2020-05-16 11:48:41 +0300

    Update chainIds.js

--- app/lib/chain/chainIds.js
+++ app/lib/chain/chainIds.js
@@ -1,5 +1,5 @@
 export default {
     MAIN_NET:
-        "de456b2f9df12e76d78883f0a9aab78bfe4bcd6d216670e357f36809a0dc5a2c",
+        "64d2b253d3d608966feba7a2cdceba38ad97156fd8ace9cd06203d82aea8ff50",
     TEST_NET: "39f5e2ede1f8bc1a3a54a7914414e3779e33193f1f5693510e73cb7a87617447"
 };
