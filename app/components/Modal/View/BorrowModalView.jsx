d2598ed1cd9212260e54a03e4d518bef9678135a
froooze <coin@mygrowled.com>
2020-05-20 00:08:14 +0200

    BorrowModalView.jsx

--- app/components/Modal/View/BorrowModalView.jsx
+++ app/components/Modal/View/BorrowModalView.jsx
@@ -66,7 +66,7 @@ export function BorrowModalView({
     const userExchangePrice = newPosition ? (
         <FormattedPrice
             noPopOver
-            noTip
+            noInvertTip
             quote_amount={maintenanceRatio * debtAmount * quotePrecision}
             quote_asset={quoteAssetObj.get("id")}
             base_asset={backingAssetObj.get("id")}
@@ -94,18 +94,20 @@ export function BorrowModalView({
                             onClick={onPayDebt.bind(this)}
                             content="borrow.pay_max_debt"
                         />
-                        &nbsp;
                     </span>
                 ) : null}
-                <Translate component="span" content="transfer.available" />:{" "}
-                <span>
+                <Translate component="span" class="nontitle" content="transfer.available" />
+                <span class="nontitle">
+                :{" "}
                     {debtBalanceObj.id ? (
                         <FormattedAsset
+                            noTip
                             amount={remainingDebtBalance}
                             asset={quoteAssetObj.get("id")}
                         />
                     ) : (
                         <FormattedAsset
+                            noTip
                             amount={0}
                             asset={quoteAssetObj.get("id")}
                         />
@@ -114,27 +116,53 @@ export function BorrowModalView({
             </span>
         </span>
     );
-
     const backingBalanceText = (
         <span>
             <span>
                 <span>
                     <Translate
-                        component="a"
-                        onClick={onMaximizeCollatereal.bind(this)}
-                        content="borrow.use_max"
+                    percentage = {25}
+                    component="a"
+                    onClick={onMaximizeCollatereal.bind(this, 25)}
+                    content="borrow.use_percentage"
                     />
-                    &nbsp;
                 </span>
-                <Translate component="span" content="transfer.available" />:{" "}
+                <span>
+                    <Translate
+                    percentage = {50}
+                    component="a"
+                    onClick={onMaximizeCollatereal.bind(this, 50)}
+                     content="borrow.use_percentage"
+                    />
+                </span>
+                    <Translate
+                    percentage = {75}
+                    component="a"
+                    onClick={onMaximizeCollatereal.bind(this, 75)}
+                    content="borrow.use_percentage"
+                    />
+                </span>
+                <span>
+                      <Translate
+                      percentage = {100}
+                      component="a"
+                      onClick={onMaximizeCollatereal.bind(this, 100)}
+                      content="borrow.use_percentage"
+                      />
+                </span>
+                      <Translate component="span" class="nontitle" content="transfer.available" />
+                <span class="nontitle">
+                      :{" "}
                 <span>
                     {collateralBalanceObj.id ? (
                         <FormattedAsset
+                            noTip
                             amount={remainingBackingBalance}
                             asset={backingAssetObj.get("id")}
                         />
                     ) : (
                         <FormattedAsset
+                            noTip
                             amount={0}
                             asset={backingAssetObj.get("id")}
                         />
@@ -190,6 +218,7 @@ export function BorrowModalView({
                             :&nbsp;
                         </span>
                         <FormattedPrice
+                            noInvertTip
                             noPopOver
                             quote_amount={asset_utils
                                 .extractRawFeedPrice(quoteAssetObj)
@@ -227,22 +256,8 @@ export function BorrowModalView({
 
             <Form className="full-width" layout="vertical">
                 <AmountSelector
-                    label="transaction.borrow_amount"
-                    amount={debtAmount.toString()}
-                    onChange={onBorrowChange.bind(this)}
-                    asset={quoteAssetObj.get("id")}
-                    assets={[quoteAssetObj.get("id")]}
-                    display_balance={bitAssetBalanceText}
-                    placeholder="0.0"
-                    tabIndex={1}
-                    lockStatus={
-                        unlockedInputType == "debt" || isRatioLocked
-                            ? false
-                            : true
-                    }
-                    onLockChange={onLockChangeDebt.bind(this)}
-                />
-                <AmountSelector
+                    noPopOver
+                    style={{marginTop: 35, marginBottom: 41}}
                     label="transaction.collateral"
                     amount={collateral.toString()}
                     onChange={onCollateralChange.bind(this)}
@@ -264,6 +279,7 @@ export function BorrowModalView({
                             : null
                     }
                 />
+
                 {!isPredictionMarket ? (
                     <React.Fragment>
                         <Row gutter={16}>
@@ -279,15 +295,9 @@ export function BorrowModalView({
                                                 ? "error"
                                                 : null
                                     }
-                                    help={
-                                        errors.close_maintenance
-                                            ? errors.close_maintenance
-                                            : errors.below_maintenance
-                                                ? errors.below_maintenance
-                                                : null
-                                    }
                                 >
                                     <Input
+                                        style={{ width: 174 }}
                                         value={
                                             collateral_ratio == 0
                                                 ? ""
@@ -311,7 +321,6 @@ export function BorrowModalView({
                                                 onClick={onLockChangeCR.bind(
                                                     this
                                                 )}
-                                                style={{fontSize: "20px"}}
                                             />
                                         }
                                     />
@@ -319,6 +328,7 @@ export function BorrowModalView({
                             </Col>
                             <Col span={12}>
                                 <Form.Item
+                                    style={{textAlign: 'right' }}
                                     validateStatus={
                                         errors.tcr_below_maintenance
                                             ? "error"
@@ -339,7 +349,7 @@ export function BorrowModalView({
                                             checked={useTargetCollateral}
                                             tabIndex={4}
                                         >
-                                            <Translate content="borrow.enable_target_collateral_ratio" />
+                                            <Translate content="borrow.target_collateral_ratio" />
                                         </Checkbox>
                                         <Tooltip
                                             title={counterpart.translate(
@@ -352,6 +362,7 @@ export function BorrowModalView({
 
                                     {useTargetCollateral ? (
                                         <Input
+                                            style={{ width: 130 }}
                                             value={
                                                 isNaN(target_collateral_ratio)
                                                     ? "0"
@@ -363,18 +374,41 @@ export function BorrowModalView({
                                             )}
                                         />
                                     ) : null}
+                                    </Form.Item>
+                                </Col>
+                                <Col span={24}>
+                                <Form.Item>
+                                    <Slider
+                                        style={{ marginTop: -20 }}
+                                        step={0.01}
+                                        min={maintenanceRatio}
+                                        max={maintenanceRatio * 4}
+                                        value={collateral_ratio}
+                                        onChange={onRatioChange.bind(this)}
+                                        tooltipPlacement="bottom"
+                                    />
+
+                                    <AmountSelector
+                                        noPopOver
+                                        style= {{ marginTop: 50, marginBottom: 0, paddingBottom: 0 }}
+                                        label="transaction.borrow_amount"
+                                        amount={debtAmount.toString()}
+                                        onChange={onBorrowChange.bind(this)}
+                                        asset={quoteAssetObj.get("id")}
+                                        assets={[quoteAssetObj.get("id")]}
+                                        display_balance={bitAssetBalanceText}
+                                        placeholder="0.0"
+                                        tabIndex={1}
+                                        lockStatus={
+                                            unlockedInputType == "debt" || isRatioLocked
+                                                ? false
+                                                : true
+                                        }
+                                        onLockChange={onLockChangeDebt.bind(this)}
+                                    />
                                 </Form.Item>
                             </Col>
                         </Row>
-                        <Form.Item label={"Ratio Slider"}>
-                            <Slider
-                                step={0.01}
-                                min={0}
-                                max={maintenanceRatio * 12}
-                                value={collateral_ratio}
-                                onChange={onRatioChange.bind(this)}
-                            />
-                        </Form.Item>
                     </React.Fragment>
                 ) : null}
             </Form>
